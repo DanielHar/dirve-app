be1f9f7f27e0ebd410aa6cb518b4ecb52889afdb
Daniel Harit <daniel.harit@gmail.com>
2019-04-02 13:18:40 +0300

    Create manager app

---
+++ manager-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline">
+        <span>NiceDrive</span>
+        <span class="font-weight-light ml-3">Driver UI</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <span class="mr-2">IDF</span>
+    </v-toolbar>
+
+    <v-content>
+      <DriverForm/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import DriverForm from './components/DriverForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DriverForm
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ manager-app/src/components/DriverForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-form>
+        <v-container>
+            <v-layout row wrap>
+                <v-flex xs12 sm6 md3>
+                    <v-text-field label="Driver ID"></v-text-field>
+                </v-flex>
+            </v-layout>
+            <v-layout row wrap>
+                <v-flex xs12 sm6 md3>
+                    <v-text-field label="Driver name"></v-text-field>
+                </v-flex>
+            </v-layout>
+            <v-layout>
+                <v-checkbox label="Water"></v-checkbox>
+            </v-layout>
+            <v-layout>
+                <v-checkbox label="Oil"></v-checkbox>
+            </v-layout>
+            <v-layout row wrap>
+                <v-flex xs12 sm6 md3>
+                    <v-text-field label="Tire Pressure"></v-text-field>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-form>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
